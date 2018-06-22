5832c1b0219808134991f05b9bccdbe25db0ac16
Alfredo <vasquez.alf@gmail.com>
2018-06-22 12:02:35 -0500

    Login simple con mail y redireccionamiento

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get'home', to: 'static_pages#home', as: :home
+  get'public', to: 'static_pages#public', as: :public
+  root :to => 'static_pages#index'
 end
--- db/migrate/20180622151014_devise_create_users.rb
+++ db/migrate/20180622151014_devise_create_users.rb
@@ -32,11 +32,26 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      ## More Fields
+      t.string :username
+      t.string :name
+      t.string :last_name
+      #t.string :doi
+      #t.string :dci
+      #t.string :pkm_id
+      #t.string :ygo_id
+      t.string :mobile
+      t.string :address_l1
+      t.string :address_l2
+      t.date :birthday
+      t.string :genre
+      t.string :roles_mask
 
       t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
+    add_index :users, :username,             unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
